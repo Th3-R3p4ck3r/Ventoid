fd29ea9aee0a28184a35c2d9296804c62b864383
Th3-R3p4ck3r <114827403+Th3-R3p4ck3r@users.noreply.github.com>
2026-08-05 16:00:42 +0300

    Update version code and name in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.ventoid.app"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.17"
+        versionCode = 1173
+        versionName = "0.1.17.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     signingConfigs {
